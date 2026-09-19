ed15339926b38d41e134e1fe6c2e7d6894c507d6
Joel <joel799659@gmail.com>
2026-09-20 01:15:06 +0530

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-API_URL = "https://fastapi-insurance-premium-prediction-04.streamlit.app"
+API_URL = "https://fastapi-insurance-premium-prediction-04.streamlit.app/predict"
 
 st.title("Insurance Premium Category Predictor")
 st.markdown("Enter your details below:")
